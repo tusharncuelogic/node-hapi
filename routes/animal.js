120efd5b2c1985f85bf33fec86d7faf30bd24bae
tusharncuelogic <tushar.nikam@cuelogic.co.in>
2016-09-28 19:24:43 +0530

    Adding changes

--- routes/animal.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(request, reply) {
-	
-    sendUserData("Animal Name",function(response){
-    	return reply(response) ;
-    });
-
-}
--- routes/names.js
+++
@@ -1,7 +0,0 @@
-module.exports.name  = "Tushar";
-module.exports.setName = function(nm){
-	this.name = nm;
-}
-module.exports.getName = function(){
-	return this.name;
-}
